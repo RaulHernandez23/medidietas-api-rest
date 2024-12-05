b466db90f3468a6ad6b042ba423828bd9d485b94
MIGUEL ANGEL MORALES CRUZ <zS21026433@estudiantes.uv.mx>
2024-12-04 23:01:50 -0600

    Se completo el CU-11 y CU-12

--- src/controllers/alimento.js
+++ src/controllers/alimento.js
@@ -1,4 +1,6 @@
+const { Op } = require("sequelize");
 const Alimento = require("../models/Alimento");
+const sequelize = require("../models/database");
 
 const obtenerAlimentos = async (req, res) => {
   try {
@@ -9,6 +11,119 @@ const obtenerAlimentos = async (req, res) => {
   }
 };
 
+const registrarAlimento = async (req, res) => {
+  const {
+    nombre,
+    calorias,
+    carbohidratos,
+    grasas,
+    imagen,
+    proteinas,
+    tamano_racion,
+    marca,
+    id_unidad_medida,
+  } = req.body;
+  const transaction = await sequelize.transaction();
+
+  try {
+    // Verificar si ya existe un alimento con el mismo nombre
+    const alimentoExistente = await Alimento.findOne({
+      where: { nombre },
+      transaction,
+    });
+    if (alimentoExistente) {
+      await transaction.rollback();
+      return res
+        .status(400)
+        .json({ error: "Ya existe un alimento con el mismo nombre" });
+    }
+
+    // Crear el alimento con estado true por defecto
+    const nuevoAlimento = await Alimento.create(
+      {
+        nombre,
+        calorias,
+        carbohidratos,
+        grasas,
+        imagen,
+        proteinas,
+        tamano_racion,
+        marca,
+        id_unidad_medida,
+        estado: true,
+      },
+      { transaction }
+    );
+
+    await transaction.commit();
+    res.status(201).json(nuevoAlimento);
+  } catch (error) {
+    await transaction.rollback();
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const editarAlimento = async (req, res) => {
+  const { id } = req.params;
+  const {
+    nombre,
+    calorias,
+    carbohidratos,
+    grasas,
+    imagen,
+    proteinas,
+    tamano_racion,
+    marca,
+    id_unidad_medida,
+  } = req.body;
+  const transaction = await sequelize.transaction();
+
+  try {
+    // Buscar el alimento por su id
+    const alimento = await Alimento.findByPk(id, { transaction });
+    if (!alimento) {
+      await transaction.rollback();
+      return res.status(404).json({ error: "Alimento no encontrado" });
+    }
+
+    // Verificar si ya existe un alimento con el mismo nombre (excluyendo el alimento actual)
+    const alimentoExistente = await Alimento.findOne({
+      where: { nombre, id: { [Op.ne]: id } },
+      transaction,
+    });
+    if (alimentoExistente) {
+      await transaction.rollback();
+      return res
+        .status(400)
+        .json({ error: "Ya existe un alimento con el mismo nombre" });
+    }
+
+    // Actualizar los datos del alimento (excluyendo el estado)
+    await alimento.update(
+      {
+        nombre,
+        calorias,
+        carbohidratos,
+        grasas,
+        imagen,
+        proteinas,
+        tamano_racion,
+        marca,
+        id_unidad_medida,
+      },
+      { transaction }
+    );
+
+    await transaction.commit();
+    res.status(200).json(alimento);
+  } catch (error) {
+    await transaction.rollback();
+    res.status(400).json({ error: error.message });
+  }
+};
+
 module.exports = {
   obtenerAlimentos,
+  registrarAlimento,
+  editarAlimento,
 };
--- src/routes/alimento.js
+++ src/routes/alimento.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const { validarJWT } = require("../helpers/validar-jwt");
 
-const { obtenerAlimentos } = require("../controllers/alimento");
+const {
+  obtenerAlimentos,
+  registrarAlimento,
+  editarAlimento,
+} = require("../controllers/alimento");
 
 const router = express.Router();
 
 router.get("/", [validarJWT], obtenerAlimentos);
+router.post("/", [validarJWT], registrarAlimento);
+router.put("/:id", [validarJWT], editarAlimento);
 
 module.exports = router;
