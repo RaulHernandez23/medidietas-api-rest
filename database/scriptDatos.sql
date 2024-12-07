834391d0cddb46463bd0c0bbad14a18c8f0a7897
Raul <raulh230600@gmail.com>
2024-12-07 14:48:05 -0600

    fix(Expertos): arreglada a nuevo nombre de columna

--- database/scriptDatos.sql
+++ database/scriptDatos.sql
@@ -37,7 +37,7 @@ INSERT INTO `momento` (`nombre`) VALUES
 ('Cena');
 
 -- Insertar datos de ejemplo en la tabla experto_nutricion
-INSERT INTO `experto_nutricion` (`nombre`, `apellido_paterno`, `apellido_materno`, `contrasena`, `correo`, `fecha_nacimiento`, `foto`, `educacion`, `perfilProfesional`) VALUES
+INSERT INTO `experto_nutricion` (`nombre`, `apellido_paterno`, `apellido_materno`, `contrasena`, `correo`, `fecha_nacimiento`, `foto`, `educacion`, `perfil_profesional`) VALUES
 ('Laura', 'Gómez', 'Sánchez', 'contrasena123', 'lgomez@ejemplo.com', '1980-04-10', NULL, 'Nutrición y Dietética', 'Especialista en nutrición deportiva'),
 ('Miguel', 'Ruiz', 'Martínez', 'contrasena456', 'mruiz@ejemplo.com', '1975-08-20', NULL, 'Ciencias de la Alimentación', 'Experto en dietas vegetarianas');
 
